b0ccddb18ed2d0868fd910e0cc7a4fe47d051b3c
Sahan Yashmika <88578622+SahanY099@users.noreply.github.com>
2024-01-06 00:45:06 +0530

    Program home page

--- home.php
+++ home.php
@@ -1,3 +1,7 @@
+<?php
+include "connection.php";
+?>
+
 <!DOCTYPE html>
 
 <head>
@@ -36,8 +40,22 @@
 
                             <select class="form-select" style="max-width: 250px;" id="basic_search_select">
                                 <option value="0">All Categories</option>
-                                <option value="1">Mobile phones</option>
-                                <option value="2">Laptops</option>
+
+                                <?php
+                                $category_q = "SELECT * FROM `category`";
+                                $category_rs = Database::search($category_q);
+                                $category_num = $category_rs->num_rows;
+
+                                for ($x = 0; $x < $category_num; $x++) {
+                                    $category_data = $category_rs->fetch_assoc();
+                                    ?>
+                                    <option value="<?php echo $category_data["cat_id"]; ?>">
+                                        <?php echo $category_data["cat_name"]; ?>
+                                    </option>
+                                    <?php
+                                }
+                                ?>
+
                             </select>
 
                         </div>
@@ -78,7 +96,7 @@
                                 </div>
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
-                                        <img src="resources/slider_images/posterimg.jpg"
+                                        <img src="resources/slider_images/posterimg.jpg" alt="Carousel Image 01"
                                             class="d-block img-thumbnail poster-img-1" />
                                         <div class="carousel-caption d-none d-md-block poster-caption">
                                             <h5 class="poster-title">Welcome to eShop</h5>
@@ -86,11 +104,11 @@
                                         </div>
                                     </div>
                                     <div class="carousel-item">
-                                        <img src="resources/slider_images/posterimg2.jpg"
+                                        <img src="resources/slider_images/posterimg2.jpg" alt="Carousel Image 02"
                                             class="d-block img-thumbnail poster-img-1" />
                                     </div>
                                     <div class="carousel-item">
-                                        <img src="resources/slider_images/posterimg3.jpg"
+                                        <img src="resources/slider_images/posterimg3.jpg" alt="Carousel Image 03"
                                             class="d-block img-thumbnail poster-img-1" />
                                         <div class="carousel-caption d-none d-md-block poster-caption-1">
                                             <h5 class="poster-title">Be Free.....</h5>
@@ -115,54 +133,133 @@
 
                     <!-- Carousel -->
 
-                    <!-- Category Name -->
+                    <?php
+                    $category_rs = Database::search($category_q);
+                    $category_num = $category_rs->num_rows;
 
-                    <div class="col-12 mt-3 mb-3">
-                        <a href="#" class="text-decoration-none text-dark fs-3 fw-bold">Mobile phones</a> &nbsp;&nbsp;
-                        <a href="#" class="text-decoration-none text-dark fs-6">See All &nbsp;&rarr;</a>
-                    </div>
+                    for ($x = 0; $x < $category_num; $x++) {
+                        $category_data = $category_rs->fetch_assoc();
+                        ?>
+                        <!-- Category Name -->
 
-                    <!-- Category Name -->
-                    <!-- products -->
-
-                    <div class="col-12 mb-3">
-                        <div class="row border border-primary">
-
-                            <div class="col-12">
-                                <div class="row justify-content-center gap-2">
-
-                                    <div class="card col-6 col-lg-2 mt-2 mb-2" style="width: 18rem;">
-
-                                        <img src="resources/mobile_images/iphone12.jpg"
-                                            class="card-img-top img-thumbnail mt-2" style="height: 180px;" />
-                                        <div class="card-body ms-0 m-0 text-center">
-                                            <h5 class="card-title fw-bold fs-6">Apple iPhone 12</h5>
-                                            <span class="badge rounded-pill text-bg-info">New</span><br />
-                                            <span class="card-text text-primary">Rs. 100000 .00</span><br />
-
-                                            <span class="card-text text-warning fw-bold">In Stock</span><br />
-                                            <span class="card-text text-success fw-bold">10 Items
-                                                Available</span><br /><br />
-                                            <a href='#' class="col-12 btn btn-success">Buy Now</a>
-
-                                            <button class="col-12 btn btn-dark mt-2">
-                                                <i class="bi bi-cart-plus-fill text-white fs-5"></i>
-                                            </button>
-
-                                            <button class="col-12 btn btn-outline-light mt-2 border border-primary">
-                                                <i class="bi bi-heart-fill text-danger fs-5"></i>
-                                            </button>
-
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-
+                        <div class="col-12 mt-3 mb-3">
+                            <a href="#" class="text-decoration-none text-dark fs-3 fw-bold">
+                                <?php echo $category_data["cat_name"]; ?>
+                            </a>
+                            &nbsp;&nbsp;
+                            <a href="#" class="text-decoration-none text-dark fs-6">See All &nbsp;&rarr;</a>
                         </div>
-                    </div>
 
-                    <!-- products -->
+                        <!-- Category Name -->
+
+                        <!-- products -->
+
+                        <div class="col-12 mb-3">
+                            <div class="row border border-primary">
+
+                                <div class="col-12">
+                                    <div class="row justify-content-center gap-2">
+
+                                        <?php
+                                        $product_rs = Database::search("SELECT *
+                                        FROM `product`
+                                        WHERE
+                                            `category_cat_id` = '" . $category_data["cat_id"] . "' AND
+                                            `status_status_id` = '1'
+                                        ORDER BY `datetime_added` DESC
+                                        LIMIT 4
+                                        OFFSET 0"
+                                        );
+                                        $product_num = $product_rs->num_rows;
+
+                                        for ($i = 0; $i < $product_num; $i++) {
+                                            $product_data = $product_rs->fetch_assoc();
+
+                                            $img_rs = Database::search("SELECT *
+                                                FROM `product_img`
+                                                WHERE
+                                                    `product_id` = '" . $product_data['id'] . "'"
+                                            );
+                                            $img_data = $img_rs->fetch_assoc();
+
+                                            ?>
+
+                                            <div class="card col-6 col-lg-2 mt-2 mb-2" style="width: 18rem;">
+
+                                                <img src="<?php echo $img_data["img_path"]; ?>" alt="Product Image"
+                                                    class="card-img-top img-thumbnail mt-2" style="height: 180px;" />
+                                                <div class="card-body ms-0 m-0 text-center">
+                                                    <h5 class="card-title fw-bold fs-6">
+                                                        <?php echo $product_data["title"]; ?>
+                                                    </h5>
+                                                    <span class="badge rounded-pill text-bg-info">New</span><br />
+                                                    <span class="card-text text-primary">
+                                                        Rs. <?php echo $product_data["price"]; ?>.00
+                                                    </span>
+                                                    <br />
+
+                                                    <?php
+                                                    if ($product_data["qty"] > 0) {
+                                                        ?>
+
+                                                        <span class="card-text text-warning fw-bold">
+                                                            In Stock
+                                                        </span><br />
+                                                        <span class="card-text text-success fw-bold">
+                                                            <?php echo $product_data["qty"]; ?> Items Available
+                                                        </span>
+                                                        <br /><br />
+                                                        <a href='#' class="col-12 btn btn-success">Buy Now</a>
+                                                        <button class="col-12 btn btn-dark mt-2">
+                                                            <i class="bi bi-cart-plus-fill text-white fs-5"></i>
+                                                        </button>
+
+                                                        <button class="col-12 btn btn-outline-light mt-2 border border-primary">
+                                                            <i class="bi bi-heart-fill text-danger fs-5"></i>
+                                                        </button>
+
+                                                        <?php
+                                                    } else {
+                                                        ?>
+
+                                                        <span class="card-text text-danger fw-bold">
+                                                            Out Of Stock
+                                                        </span>
+                                                        <br />
+                                                        <br />
+                                                        <a href='#' class="col-12 btn btn-success disabled">Buy Now</a>
+
+                                                        <button class="col-12 btn btn-dark mt-2 disabled">
+                                                            <i class="bi bi-cart-plus-fill text-white fs-5"></i>
+                                                        </button>
+
+                                                        <button class="col-12 btn btn-outline-light mt-2 border border-primary">
+                                                            <i class="bi bi-heart-fill text-danger fs-5"></i>
+                                                        </button>
+
+                                                        <?php
+                                                    }
+                                                    ?>
+
+                                                </div>
+                                            </div>
+
+                                            <?php
+                                        }
+                                        ?>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <!-- products -->
+                        <?php
+                    }
+                    ?>
+
+
+
 
                 </div>
             </div>
